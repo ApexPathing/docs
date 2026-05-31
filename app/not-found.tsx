830b1673e9c2024676501e745ff672e2b565da2b
DylDev <dylanb92010@gmail.com>
2026-05-31 11:16:35 -0400

    Fix build issue

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Not Found',
+};
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
